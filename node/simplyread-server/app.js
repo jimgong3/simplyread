3f277824203ca8dda46abdb5a2c3992da7c30f79
jimgong <jimgong3@gmail.com>
2017-09-20 11:21:58 +0800

    Revert "change log directory"
    This reverts commit 529cfcf4e8561a386c8b705329d9e1d1bf68820f.

--- node/simplyread-server/app.js
+++ node/simplyread-server/app.js
@@ -1,6 +1,4 @@
 var app = require('express')();
-app.set('view engine', 'pug')
-
 var pretty = require('express-prettify');
 var assert = require('assert');
 var mongoQuery = require('./mongoQuery');
@@ -10,13 +8,12 @@ var multiparty = require('multiparty');
 // var multipartMiddleware = multiparty();
 var util = require('util')
 var fs = require('fs')
-var web = require('./web');
 
 var winston = require('winston')
 var logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)(),
-    new (winston.transports.File)({ filename: './logs/app.log' })
+    new (winston.transports.File)({ filename: 'app.log' })
   ]
 });
 
@@ -198,7 +195,7 @@ app.get('/addNewBook', function (req, res) {
 });
 
 app.post('/upload', function(req, res) {
-	logger.info("app>> upload");
+	  logger.info("app>> upload");
   	logger.info("app>> req body: ")
   	logger.info(req.body);
   	logger.info("app>> req files: ")
@@ -221,10 +218,6 @@ app.post('/upload', function(req, res) {
       res.end(util.inspect({fields: fields, files: files}));
     });
 
-});
-
-app.get('/web/pug', function (req, res) {
-  res.render('index', {title: 'hey', message: 'hello there from pug'});
 });
 
 app.listen(port, function () {
