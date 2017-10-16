b7a1da79889b00d042bed4898a2bd5a723290015
jim <jimgong3@gmail.com>
2017-10-16 09:20:46 +0800

    register

--- node/simplyread-server/loginUtil.js
+++ node/simplyread-server/loginUtil.js
@@ -6,6 +6,8 @@ var logger = new (winston.Logger)({
   ]
 });
 
+// CLIENT FACING FUNCTION
+// Return: [user] for success, or [] otherwise
 exports.register = function(req, db, callback){
   logger.info("loginUtil>> register ");
 
@@ -13,40 +15,36 @@ exports.register = function(req, db, callback){
   var fullname = req.body.fullname;
   var password = req.body.password;
   var email = req.body.email;
-  var shipping_address = req.body.shipping_address;
   logger.info("loginUtil>> username: " + username + ", fullname: " + fullname + ", password: " + password
-				+ ", email: " + email + ", shipping address: " + shipping_address);
-	
+				+ ", email: " + email);
+
   var collection = db.collection('users');
   var query = {username: username};
   logger.info("mongoQuery>> query: ");
   logger.info(query);
 
   collection.find(query).toArray(function(err, docs) {
-	if(docs.length > 0){
-		logger.error("loginUtil>> error: username already exist, return empty");
-		var result = [];
-		callback(result);
-	} else {
-		var user = {};
-		user.username = username;
-		user.password = password;
-		if(fullname != null){
-			user.fullname = fullname;
-		}
-		if(email != null){
-			user.email = email;
-		}
-		if(shipping_address != null){
-			user.shipping_address = shipping_address;
-		}
-		
-    	collection.insertOne(user, function(err, docs) {
-		  logger.info("mongoQuery>> 1 user inserted");
-		  var result = [];
-		  result.push(user);
-		  callback(result);
-	  });
-	}
+  	if(docs.length > 0){
+  		logger.error("loginUtil>> error: username already exist, return empty");
+  		var result = [];
+  		callback(result);
+  	} else {
+    		var user = {};
+    		user.username = username;
+    		user.password2 = password;  //use password2 for encrypted password
+    		if(fullname != null){
+    			user.fullname = fullname;
+    		}
+    		if(email != null){
+    			user.email = email;
+    		}
+
+      	collection.insertOne(user, function(err, docs) {
+  		  logger.info("mongoQuery>> 1 user inserted");
+  		  var result = [];
+  		  result.push(user);
+  		  callback(result);
+  	  });
+  	}
   });
 }
