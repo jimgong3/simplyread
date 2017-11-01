c2c776f99b03dcb20bc5a51cb20c671078672a9a
jim <jimgong3@gmail.com>
2017-11-01 14:48:01 +0800

    manage order & book status

--- node/simplyread-server/ordersUtil.js
+++ node/simplyread-server/ordersUtil.js
@@ -89,7 +89,8 @@ exports.submitOrder = function(req, db, callback){
   		callback(orderJson);
 
       processNewOrder(orderJson, db);
-
+      updateBookStatusOrdered(orderId, db);
+      
   		if(orderJson.email != null) {
   		  logger.info("ordersUtil>> send email order confirmation to: " + orderJson.email);
   		  sendOrderConfirmation(orderJson.email, orderJson);
@@ -228,7 +229,7 @@ function generateCashTxn(orderJson, db, callback){
 		var txn4 = {};
 		txn4["date"] = orderJson["date"];
 		txn4["account"] = book["owner"];
-    txn4["amount"] = "+" + book["price"];
+    txn4["amount"] = "+" + book["sr_price"];
 		txn4["description"] = "圖書被借出，收取借閱費， 訂單號碼：" + orderJson["orderId"];
 		transactions.push(txn4);
     logger.info("ordersUtil>> txn4: " + JSON.stringify(txn4));
@@ -286,48 +287,49 @@ function updateBalances(transactions, db){
   for(var i=0; i<transactions.length; i++){
     var txn = transactions[i];
     logger.info("ordersUtil>> processing txn: " + JSON.stringify(txn));
-    updateBalanceForTxn(txn, db);
+    // updateBalanceForTxn(txn, db);
     updateUserProfileBalanceForTxn(txn, db);
   }
 }
 
+// Obsolete, the collection "balance" is redundant, use user profile instead
 // Sub-function of processNewOrder
-function updateBalanceForTxn(txn, db){
-  logger.info("ordersUtil>> updateBalanceForTxn start...");
-
-  var username = txn["account"];
-  var query = {username: username};
-  var collection = db.collection("balances");
-
-  collection.find(query).toArray(function(err, docs){
-    if(docs.length == 0) {
-      logger.error("ordersUtil>> this shall not happen - each user should have a balance a/c in place: " + username);
-    } else {
-      var amount = txn["amount"];
-      var flag = amount[0]; //+/-
-      var amountInt = parseInt(amount.substring(1), 10);
-      if (flag == '-')
-        amountInt = 0 - amountInt;
-
-      var update = {$inc: {balance: amountInt}};
-      logger.info("ordersUtil>> txn: " + JSON.stringify(txn));          //for ref
-      logger.info("ordersUtil>> update: " + JSON.stringify(update));
-      collection.update(query, update, function(err, result){
-        logger.info("ordersUtil>> 1 record updated");
-      })
-    }
-  });
-}
+// function updateBalanceForTxn(txn, db){
+//   logger.info("ordersUtil>> updateBalanceForTxn start...");
+//
+//   var username = txn["account"];
+//   var query = {username: username};
+//   var collection = db.collection("balances");
+//
+//   collection.find(query).toArray(function(err, docs){
+//     if(docs.length == 0) {
+//       logger.error("ordersUtil>> this shall not happen - each user should have a balance a/c in place: " + username);
+//     } else {
+//       var amount = txn["amount"];
+//       var flag = amount[0]; //+/-
+//       var amountInt = parseInt(amount.substring(1), 10);
+//       if (flag == '-')
+//         amountInt = 0 - amountInt;
+//
+//       var update = {$inc: {balance: amountInt}};
+//       logger.info("ordersUtil>> txn: " + JSON.stringify(txn));          //for ref
+//       logger.info("ordersUtil>> update: " + JSON.stringify(update));
+//       collection.update(query, update, function(err, result){
+//         logger.info("ordersUtil>> 1 record updated");
+//       })
+//     }
+//   });
+// }
 
 // Sub-function of processNewOrder
 function updateUserProfileBalanceForTxn(txn, db){
   logger.info("ordersUtil>> updateUserProfileBalanceForTxn start...");
 
   var username = txn["account"];
-  if(username.indexOf("simplyread")>-1){
-	  logger.info("ordersUtil>> system account, skip update user profile: " + username);
-	  return;
-  }
+  // if(username.indexOf("simplyread")>-1){
+	//   logger.info("ordersUtil>> system account, skip update user profile: " + username);
+	//   return;
+  // }
 
   var query = {username: username};
   var collection = db.collection("users");
@@ -393,6 +395,45 @@ function sendOrderConfirmation(email, orderJson){
       logger.info('ordersUtil>> Email sent: ' + info.response);
     }
   });
+}
+
+
+// Sub-function of submitOrder
+// For each book in the oder, change:
+//    hold_by:    N/A
+//    status:     delivered
+function updateBookStatusOrdered(orderId, db){
+	logger.info("ordersUtil>> updateBookStatusOrdered start...");
+
+	var collection = db.collection('orders');
+	var query = {orderId: orderId};
+	collection.find(query).toArray(function(err, docs){
+		if(docs.length == 0){
+			logger.info("ordersUtil>> this shall not happen, orderId not found: " + orderId);
+		} else {
+			var order = docs[0];
+			logger.info("ordersUtil>> order: " + JSON.stringify(order));
+
+			var books = order["books"];
+			logger.info("ordersUtil>> # of books in order: " + books.length);
+
+			for(var i=0; i<books.length; i++){
+				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
+				var curBookId = books[i]["_id"];
+				var query2 = {_id: ObjectId(curBookId)};
+				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
+
+				var status = "已預訂";
+				var update = {$set: {status: status}};
+				logger.info("ordersUtil>> update: " + JSON.stringify(update));
+
+				var collectionBooks = db.collection('books');
+				collectionBooks.update(query2, update, function(err, docs){
+					logger.info("ordersUtil>> book status updated: " + curBookId);
+				});
+			}
+		}
+	});
 }
 
 // CLIENT FACING FUNCTION
@@ -466,6 +507,9 @@ exports.orderDelivered = function(req, db, callback){
 
 
 // Sub-function of orderDelivered
+// For each book in the oder, change:
+//    hold_by:    N/A
+//    status:     delivered
 function updateBookStatusDelivered(orderId, db){
 	logger.info("ordersUtil>> updateBookStatusDelivered start...");
 
@@ -486,7 +530,7 @@ function updateBookStatusDelivered(orderId, db){
 				var curBookId = books[i]["_id"];
 				var query2 = {_id: ObjectId(curBookId)};
 				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
-				
+
 				var hold_by = "N/A";
 				var status = "已送出";
 				var update = {$set: {hold_by: hold_by, status: status}};
@@ -504,49 +548,50 @@ function updateBookStatusDelivered(orderId, db){
 // Sub-function of orderDelivered
 // After the order is marked as delivered, remove the book from the holder's bookshelf
 // which will be added to the next reader's bookshelf upon he receives
-function removeBooksFromHolderBookshelf(orderId, db){
-	logger.info("ordersUtil>> removeBooksFromHolderBookshelf start...");
+// function removeBooksFromHolderBookshelf(orderId, db){
+// 	logger.info("ordersUtil>> removeBooksFromHolderBookshelf start...");
+//
+// 	var collection = db.collection('orders');
+// 	var query = {orderId: orderId};
+// 	collection.find(query).toArray(function(err, docs){
+// 		if(docs.length == 0){
+// 			logger.info("ordersUtil>> this shall not happen, orderId not found: " + orderId);
+// 		} else {
+// 			var order = docs[0];
+// 			logger.info("ordersUtil>> order: " + JSON.stringify(order));
+//
+// 			var books = order["books"];
+// 			logger.info("ordersUtil>> # of books in order: " + books.length);
+//
+// 			for(var i=0; i<books.length; i++){
+// 				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
+// 				var curBookId = books[i]["book_id"];
+// 				var hold_by = books[i]["hold_by"];
+//
+// 				var collectionBooks = db.collection('books');
+// 				var query2 = {_id: ObjectId(curBookId)};
+// 				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
+//
+// 				collectionBooks.find(query2).toArray(function(err, docs){
+// 					var book = docs[0]; 	//should return only one book
+// 					var book_copies = book["book_copies"];
+// 					var status = "已送出";
+// 					book_copies[0]["status"] = status;
+//
+// 					var update = {$set: {book_copies: book_copies}};
+// 					logger.info("ordersUtil>> update: " + JSON.stringify(update));
+// 					collectionBooks.update(query2, update, function(err, docs){
+// 						logger.info("ordersUtil>> 1 book removed from holder's bookshelf");
+// 						logger.info("ordersUtil>> result: " + JSON.stringify(docs));
+// 					});
+// 				});
+// 			}
+// 		}
+// 	});
+// }
 
-	var collection = db.collection('orders');
-	var query = {orderId: orderId};
-	collection.find(query).toArray(function(err, docs){
-		if(docs.length == 0){
-			logger.info("ordersUtil>> this shall not happen, orderId not found: " + orderId);
-		} else {
-			var order = docs[0];
-			logger.info("ordersUtil>> order: " + JSON.stringify(order));
 
-			var books = order["books"];
-			logger.info("ordersUtil>> # of books in order: " + books.length);
-
-			for(var i=0; i<books.length; i++){
-				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
-				var curBookId = books[i]["book_id"];
-				var hold_by = books[i]["hold_by"];
-
-				var collectionBooks = db.collection('books');
-				var query2 = {_id: ObjectId(curBookId)};
-				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
-
-				collectionBooks.find(query2).toArray(function(err, docs){
-					var book = docs[0]; 	//should return only one book
-					var book_copies = book["book_copies"];
-					var status = "已送出";
-					book_copies[0]["status"] = status;
-
-					var update = {$set: {book_copies: book_copies}};
-					logger.info("ordersUtil>> update: " + JSON.stringify(update));
-					collectionBooks.update(query2, update, function(err, docs){
-						logger.info("ordersUtil>> 1 book removed from holder's bookshelf");
-						logger.info("ordersUtil>> result: " + JSON.stringify(docs));
-					});
-				});
-			}
-		}
-	});
-}
-
-
+// To be revised...
 // CLIENT FACING FUNCTION
 // Parameters:
 // 	- orderId
@@ -568,14 +613,18 @@ exports.orderReceived = function(req, db, callback){
     logger.info("ordersUtil>> 1 order status updated" );
     callback(docs);
 
-	addBooksToBorrowerBookshelf(orderId, db);
+	// addBooksToBorrowerBookshelf(orderId, db);
+    updateBookStatusReceived(orderId, db);
   });
 }
 
 
 // Sub-function of orderDelivered
-function addBooksToBorrowerBookshelf(orderId, db){
-	logger.info("ordersUtil>> addBooksToBorrowerBookshelf start...");
+// For each book in the oder, change:
+//    hold_by:    borrower
+//    status:     reading
+function updateBookStatusReceived(orderId, db){
+	logger.info("ordersUtil>> updateBookStatusReceived start...");
 
 	var collection = db.collection('orders');
 	var query = {orderId: orderId};
@@ -585,33 +634,143 @@ function addBooksToBorrowerBookshelf(orderId, db){
 		} else {
 			var order = docs[0];
 			logger.info("ordersUtil>> order: " + JSON.stringify(order));
-			
-			var username = order.username;
+
+      var username = order["username"];
+
 			var books = order["books"];
 			logger.info("ordersUtil>> # of books in order: " + books.length);
 
 			for(var i=0; i<books.length; i++){
 				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
-				var curBookId = books[i]["book_id"];
-
-				var collectionBooks = db.collection('books');
+				var curBookId = books[i]["_id"];
 				var query2 = {_id: ObjectId(curBookId)};
 				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
 
-				collectionBooks.find(query2).toArray(function(err, docs){
-					var book = docs[0]; 	//should return only one book
-					var book_copies = book["book_copies"];
-					var status = "閱讀中";
-					book_copies[0]["status"] = status;
-					book_copies[0]["hold_by"] = username;
+				var hold_by = username;
+				var status = "閱讀中";
+				var update = {$set: {hold_by: hold_by, status: status}};
+				logger.info("ordersUtil>> update: " + JSON.stringify(update));
 
-					var update = {$set: {book_copies: book_copies}};
-					logger.info("ordersUtil>> update: " + JSON.stringify(update));
-					
-					collectionBooks.update(query2, update, function(err, docs){
-						logger.info("ordersUtil>> 1 book received");
-						logger.info("ordersUtil>> result: " + JSON.stringify(docs));
-					});
+				var collectionBooks = db.collection('books');
+				collectionBooks.update(query2, update, function(err, docs){
+					logger.info("ordersUtil>> book status updated: " + curBookId);
+				});
+			}
+		}
+	});
+}
+
+
+// Obsolete
+// Sub-function of orderDelivered
+// function addBooksToBorrowerBookshelf(orderId, db){
+// 	logger.info("ordersUtil>> addBooksToBorrowerBookshelf start...");
+//
+// 	var collection = db.collection('orders');
+// 	var query = {orderId: orderId};
+// 	collection.find(query).toArray(function(err, docs){
+// 		if(docs.length == 0){
+// 			logger.info("ordersUtil>> this shall not happen, orderId not found: " + orderId);
+// 		} else {
+// 			var order = docs[0];
+// 			logger.info("ordersUtil>> order: " + JSON.stringify(order));
+//
+// 			var username = order.username;
+// 			var books = order["books"];
+// 			logger.info("ordersUtil>> # of books in order: " + books.length);
+//
+// 			for(var i=0; i<books.length; i++){
+// 				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
+// 				var curBookId = books[i]["book_id"];
+//
+// 				var collectionBooks = db.collection('books');
+// 				var query2 = {_id: ObjectId(curBookId)};
+// 				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
+//
+// 				collectionBooks.find(query2).toArray(function(err, docs){
+// 					var book = docs[0]; 	//should return only one book
+// 					var book_copies = book["book_copies"];
+// 					var status = "閱讀中";
+// 					book_copies[0]["status"] = status;
+// 					book_copies[0]["hold_by"] = username;
+//
+// 					var update = {$set: {book_copies: book_copies}};
+// 					logger.info("ordersUtil>> update: " + JSON.stringify(update));
+//
+// 					collectionBooks.update(query2, update, function(err, docs){
+// 						logger.info("ordersUtil>> 1 book received");
+// 						logger.info("ordersUtil>> result: " + JSON.stringify(docs));
+// 					});
+// 				});
+// 			}
+// 		}
+// 	});
+// }
+
+
+
+// CLIENT FACING FUNCTION
+// Parameters:
+// 	- orderId
+exports.orderClosed = function(req, db, callback){
+  logger.info("ordersUtil>> orderClosed start...");
+
+  var orderId = req.body.orderId;
+  logger.info("ordersUtil>> orderId: " + orderId);
+
+  var collection = db.collection('orders');
+  var query = {orderId: orderId};
+  logger.info("ordersUtil>> query: " + JSON.stringify(query));
+
+  var status = "已結束";
+  var update = {$set: {status: status}};
+  logger.info("ordersUtil>> udpate: " + JSON.stringify(update));
+
+  collection.update(query, update, function(err, docs) {
+    logger.info("ordersUtil>> 1 order status updated" );
+    callback(docs);
+
+	// addBooksToBorrowerBookshelf(orderId, db);
+    updateBookStatusClosed(orderId, db);
+  });
+}
+
+
+// Sub-function of orderClosed
+// For each book in the oder, change:
+//    hold_by:    borrower
+//    status:     idle
+function updateBookStatusClosed(orderId, db){
+	logger.info("ordersUtil>> updateBookStatusClosed start...");
+
+	var collection = db.collection('orders');
+	var query = {orderId: orderId};
+	collection.find(query).toArray(function(err, docs){
+		if(docs.length == 0){
+			logger.info("ordersUtil>> this shall not happen, orderId not found: " + orderId);
+		} else {
+			var order = docs[0];
+			logger.info("ordersUtil>> order: " + JSON.stringify(order));
+
+      var username = order["username"];
+
+			var books = order["books"];
+			logger.info("ordersUtil>> # of books in order: " + books.length);
+
+			for(var i=0; i<books.length; i++){
+				logger.info("ordersUtil>> found book id: " + books[i]["book_id"]);
+				var curBookId = books[i]["_id"];
+				var query2 = {_id: ObjectId(curBookId)};
+				logger.info("ordersUtil>> query2: " + JSON.stringify(query2));
+
+				var hold_by = username;
+				var status = "可借閱";
+				var update = {$set: {hold_by: hold_by, status: status}};
+				logger.info("ordersUtil>> update: " + JSON.stringify(update));
+
+				var collectionBooks = db.collection('books');
+				collectionBooks.update(query2, update, function(err, docs){
+					logger.info("ordersUtil>> book status updated: " + curBookId);
 				});
 			}
 		}
